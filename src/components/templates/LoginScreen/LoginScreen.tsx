f1434d75ded2c81f72c22eb37f8fc6dd0ca3d623
Kris <kris@gnhearing.com>
2023-12-25 09:27:14 +0100

    app title change

--- src/components/templates/LoginScreen/LoginScreen.tsx
+++ src/components/templates/LoginScreen/LoginScreen.tsx
@@ -4,7 +4,7 @@ export default function LoginScreen() {
   return (
     <div className="flex flex-col items-center justify-center gap-48">
       <p className="text-left text-4xl font-black text-rose-200">
-        LFBudget App
+        Lam's Family Budget App
       </p>
       <button
         className="flex h-fit w-fit items-center justify-center gap-4 rounded-2xl bg-rose-900 px-4 py-1 text-left text-2xl font-black text-rose-200 shadow-md"
